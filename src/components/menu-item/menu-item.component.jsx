a073dc61a96ed5a1c2a7628a7a032c6765f36fac
Akshat0102 <76106653+Akshat0102@users.noreply.github.com>
2022-04-16 16:00:23 +0530

    added directory.json and updated directory component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title }) => {
+const MenuItem = ({ title, imageUrl }) => {
     return (
-        <div className="menu-item">
+        <div className="menu-items" style={{
+            backgroundImage: `url(${imageUrl})`
+        }}>
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">SHOP NOW</span>
