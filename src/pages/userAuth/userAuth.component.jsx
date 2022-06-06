9c42e8b1a0a3e6f88e7e8179b881dd3bd05aa600
Akshat0102 <76106653+Akshat0102@users.noreply.github.com>
2022-06-06 16:54:09 +0530

    created signup component and enables email and password signin

--- src/pages/userAuth/userAuth.component.jsx
+++ src/pages/userAuth/userAuth.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './userAuth.styles.scss'
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/signup.component';
 
 const UserAuthPage = () => {
 
     return (
         <div className="userAuth">
             <SignIn />
+            <SignUp />
         </div>
     )
 }
